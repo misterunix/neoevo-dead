98d50c40a60dcceac356545720634eb1c0129a3f
Bill Jones <misterunix@gmail.com>
2021-11-25 11:58:55 -0600

    quick save

---
+++ rnd.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"math/big"
+
+	"crypto/rand"
+)
+
+func randFloat() float64 {
+	nBig, err := rand.Int(rand.Reader, big.NewInt(1000000))
+	if err != nil {
+		log.Println(err)
+	}
+	t := nBig.Int64()
+	r := float64(t) / 1000000.0
+	return r
+}
+
+// randFullRange : Return a float64 in the range of > -1.0 and < +1.0
+func randFloatFullValue() float64 {
+	nBig, err := rand.Int(rand.Reader, big.NewInt(1000000))
+	if err != nil {
+		log.Println(err)
+	}
+	t := nBig.Int64()
+	r := ((float64(t) / 1000000.0) * 2.0) - 1.0
+	return r
+}
+
+func randInt(max int) int {
+	t := int64(max)
+	nBig, err := rand.Int(rand.Reader, big.NewInt(t))
+	if err != nil {
+		log.Println(err)
+	}
+	return int(nBig.Int64())
+}
