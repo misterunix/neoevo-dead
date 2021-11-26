adf963e157b079b42be09f3dcfa38624d33be969
Bill Jones <misterunix@gmail.com>
2021-11-25 23:55:48 -0600

    started inputs

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ type NeoBase struct {
 	LocationY int
 	Inputs    []float64
 	Outputs   []float64
+	Hunger    int
 }
 
 var Neo []NeoBase
@@ -35,20 +36,36 @@ var inputcount int
 var hiddencount int
 var outputcount int
 
+var WorldX int
+var WorldY int
+
+var Reset bool
+
+var StepsPerGeneration int
+var CurrentStep int
+
 func main() {
+
+	WorldX = 128
+	WorldY = 128
 
 	inputcount = 5
 	hiddencount = 3
 	outputcount = 4
 	BaseNeoCount = 10
 	genecount := 8
+	StepsPerGeneration = 300
+	CurrentStep = 0
+	maxhunger := 30
 
 	Neo = make([]NeoBase, BaseNeoCount)
 	for i := 0; i < BaseNeoCount; i++ {
 		Neo[i].Genes = make([]int, genecount)
-		Neo[i].Inputs = make([]float64, 20)
+		Neo[i].Inputs = make([]float64, 12)
 		Neo[i].Outputs = make([]float64, 20)
 	}
+
+	Reset = true
 
 	if Generation == 0 {
 		for i := 0; i < BaseNeoCount; i++ {
@@ -61,6 +78,23 @@ func main() {
 				}
 			}
 		}
+	}
+
+	if Reset {
+		for i := range Neo {
+			Neo[i].LocationX = randInt(128)
+			Neo[i].LocationY = randInt(128)
+			Neo[i].Hunger = maxhunger
+		}
+	}
+
+	for i := range Neo {
+		distanceFromNorth := float64(WorldY-Neo[i].LocationY)/float64(WorldY)*2.0 - 1.0
+		distanceFromWest := float64(WorldX-Neo[i].LocationX)/float64(WorldX)*2.0 - 1.0
+		Neo[i].Inputs[0] = float64(CurrentStep) / float64(StepsPerGeneration)
+		Neo[i].Inputs[3] = distanceFromNorth
+		Neo[i].Inputs[4] = distanceFromWest
+		Neo[i].Inputs[7] = float64(Neo[i].Hunger) / float64(maxhunger)
 	}
 
 	for i := 0; i < BaseNeoCount; i++ {
@@ -84,16 +118,12 @@ func main() {
 
 }
 
-
-func Step( id int ) error {
+func Step(id int) error {
 	if id >= BaseNeoCount || id < 0 {
 		return fmt.Errorf("Step id '%d' is out of bounds", id)
 	}
 
-// Move envor inputs into neurons
-for
+	// Move envor inputs into neurons
 
-
-
-return nil
+	return nil
 }
